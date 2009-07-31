a62db3ab4cf4972c1cee85b10fcd3d5c399615ce
Stephen Woods <saw@yahoo-inc.com>
2009-07-31 13:16:49 -0700

    format for clarity

--- multiply.js
+++ multiply.js
@@ -1,1 +1,13 @@
-var L=20,r=[],c=",",h=["x"];for(var i=1,a=[];i<=L;i++){h[i]=i;for(var j=1;j<=L;j++){a[j-1]=i*j;}r[i]=a.join(c);}r[0]=h.join(c);print(r.join("\n"));
+var L = 20,
+r = [],
+c = ",",
+h = ["x"];
+for (var i = 1, a = []; i <= L; i++) {
+    h[i] = i;
+    for (var j = 1; j <= L; j++) {
+        a[j - 1] = i * j;
+    }
+    r[i] = a.join(c);
+}
+r[0] = h.join(c);
+print(r.join("\n"));
